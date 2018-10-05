3906df1466d2792e51753edcde0d2fd5d6463081
bj <bj@126.com>
2018-10-05 13:50:40 +0800

    first commit

---
+++ src/main/java/com/wkcto/maven/Demo.java
@@ -0,0 +1,17 @@
+package com.wkcto.maven;
+
+/**
+ * ClassName:Demo
+ * Package:com.wkcto.maven
+ * Description:
+ *
+ * @Date 2018/10/5 13:49
+ * @Author guoxin
+ * @Email:41700175@qq.com
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Demo for Git!!");
+    }
+}
